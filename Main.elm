ae3675cfacc79ec41ce40f9881bd5ddefa4f6024
john kelly <johnkelly@codehs.com>
2015-10-04 21:27:33 -0700

    fix indent

--- Main.elm
+++ Main.elm
@@ -42,97 +42,99 @@ type State
 ---- VIEW ----
 view : Signal.Address Action -> Model -> Html.Html
 view address model =
-    case model.state of
-        Setup -> setupControlsView address model.player
-        Play ->
-            Html.div []
-                [ Html.div [] [ Html.text (toString model) ] ]
-        GameOver ->
-            Html.div []
-                [ Html.div [] [ Html.text (toString model) ] ]
+  case model.state of
+    Setup -> setupControlsView address model.player
+    Play ->
+      Html.div []
+        [ Html.div [] [ Html.text (toString model) ] ]
+    GameOver ->
+      Html.div []
+        [ Html.div [] [ Html.text (toString model) ] ]
 
 
 setupControlsView : Signal.Address Action -> Player.Player -> Html.Html
 setupControlsView address player =
-    let html = player.fleet
-      |> Fleet.toList
-      |> List.map (\ship -> shipFieldView address ship)
-    in
-    Html.div [] (html ++ [Html.text (toString player.fleet)])
+  let
+  html = player.fleet
+    |> Fleet.toList
+    |> List.map (\ship -> shipFieldView address ship)
+  in
+  Html.div [] (html ++ [Html.text (toString player.fleet)])
 
 -- Depending on the Action render the proper html input.
 shipFieldView : Signal.Address Action -> Ship.Ship -> Html.Html
 shipFieldView address ship =
-    if not ship.added then
-        Html.div []
-        [ Html.input -- Ship Row Input
-            [ Html.Attributes.value (toString (Location.row ship.location))
-            , Html.Events.on "input" Html.Events.targetValue (Signal.message address << SetupRowField ship.id)
-            ]
-            []
-        , Html.input -- Ship Column Input
-            [ Html.Attributes.value (toString (Location.column ship.location))
-            , Html.Events.on "input" Html.Events.targetValue (Signal.message address << SetupColumnField ship.id)
-            ]
-            []
-        , Html.input -- Ship Orientation
-            [ Html.Attributes.type' "radio"
-            , Html.Attributes.checked (ship.orientation == Ship.Horizontal)
-            , Html.Events.onClick address (SetupOrientationField ship.id)
-            ]
-            []
-        , Html.button -- Add Ship
-            [ Html.Events.onClick address (SetupAddShip ship.id)
-            ]
-            []
-        ]
-    else Html.div [] []
+  if not ship.added then
+    Html.div []
+    [ Html.input -- Ship Row Input
+      [ Html.Attributes.value (toString (Location.row ship.location))
+      , Html.Events.on "input" Html.Events.targetValue (Signal.message address << SetupRowField ship.id)
+      ]
+      []
+    , Html.input -- Ship Column Input
+      [ Html.Attributes.value (toString (Location.column ship.location))
+      , Html.Events.on "input" Html.Events.targetValue (Signal.message address << SetupColumnField ship.id)
+      ]
+      []
+    , Html.input -- Ship Orientation
+      [ Html.Attributes.type' "radio"
+      , Html.Attributes.checked (ship.orientation == Ship.Horizontal)
+      , Html.Events.onClick address (SetupOrientationField ship.id)
+      ]
+      []
+    , Html.button -- Add Ship
+      [ Html.Events.onClick address (SetupAddShip ship.id)
+      ]
+      []
+    ]
+  else Html.div [] []
 
 ---- UPDATE ----
 type Action
-    = SetupOrientationField Int
-    | SetupRowField Int String
-    | SetupColumnField Int String
-    | SetupAddShip Int
-    | PlayShoot
+  = SetupOrientationField Int
+  | SetupRowField Int String
+  | SetupColumnField Int String
+  | SetupAddShip Int
+  | PlayShoot
 
 update : Action -> Model -> Model
 update action model =
-    case action of
-        SetupOrientationField shipId ->
-            let
-            player = model.player
-            updateShip ship =
-              if ship.id == shipId then
-                { ship | orientation <- toggleOrientation ship.orientation }
-              else
-                ship
-            in
-            { model | player <- { player | fleet <- (Fleet.map updateShip player.fleet) } }
-        SetupRowField shipId rowAsString ->
-            let
-            player = model.player
+  case action of
+    SetupOrientationField shipId ->
+      let
+      player = model.player
 
-            updateShip ship =
-              if ship.id == shipId then
-                Ship.setRow (toIntOrDefaultOrZero rowAsString (Ship.getRow ship)) ship
-              else
-                ship
-            in
-            { model | player <- { player | fleet <- (Fleet.map updateShip player.fleet) } }
-        SetupColumnField shipId columnAsString ->
-            let
-            player = model.player
+      updateShip ship =
+        if ship.id == shipId then
+          { ship | orientation <- toggleOrientation ship.orientation }
+        else
+          ship
+      in
+      { model | player <- { player | fleet <- (Fleet.map updateShip player.fleet) } }
+    SetupRowField shipId rowAsString ->
+      let
+      player = model.player
 
-            updateShip ship =
-              if ship.id == shipId then
-                Ship.setColumn (toIntOrDefaultOrZero columnAsString (Ship.getColumn ship)) ship
-              else
-                ship
-            in
-            { model | player <- { player | fleet <- (Fleet.map updateShip player.fleet) } }
-        SetupAddShip shipId ->
-            { model | player <- addShip shipId model.player}
+      updateShip ship =
+        if ship.id == shipId then
+          Ship.setRow (toIntOrDefaultOrZero rowAsString (Ship.getRow ship)) ship
+        else
+          ship
+      in
+      { model | player <- { player | fleet <- (Fleet.map updateShip player.fleet) } }
+    SetupColumnField shipId columnAsString ->
+      let
+      player = model.player
+
+      updateShip ship =
+        if ship.id == shipId then
+          Ship.setColumn (toIntOrDefaultOrZero columnAsString (Ship.getColumn ship)) ship
+        else
+          ship
+      in
+      { model | player <- { player | fleet <- (Fleet.map updateShip player.fleet) } }
+    SetupAddShip shipId ->
+      { model | player <- addShip shipId model.player}
 
 toggleOrientation : Ship.Orientation -> Ship.Orientation
 toggleOrientation orientation =
